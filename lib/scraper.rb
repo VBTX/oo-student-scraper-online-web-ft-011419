db9cf84e2eb7cab6286816e0d5b581fa0a1f3171
Victoria B <viktorija.bourassa@gmail.com>
2019-02-02 03:37:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     student_hashes =[]
+    hash = {}
     #
 doc.css.(".student-card").collect do |card|
-student_hash[:name] = card.css(".student-name").text
-student_hash[:location] = card.css(".student-location").text
-student_hash[:profile_url] = card.css("a").attribute("href")
+hash[:name] = card.css(".student-name").text
+hash[:location] = card.css(".student-location").text
+hash[:profile_url] = card.css("a").attribute("href")
   binding.pry
 end
 student_hash
